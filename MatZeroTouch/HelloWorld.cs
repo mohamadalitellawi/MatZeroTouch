783b509b1e0bdee7234bd2c51ded56623e29ace8
Ali <mohamadalitellawi@gmail.com>
2021-09-18 19:21:17 +0300

    Hello World example completed and tested for revit 2019 - dynamo 1.3

--- MatZeroTouch/HelloWorld.cs
+++ MatZeroTouch/HelloWorld.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace MatZeroTouch
 {
+
+
     public class HelloWorld
     {
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value; }
+        }
+
+        public HelloWorld(string message)
+        {
+            _message = message;
+        }
+
+        public bool Contains(string Substring)
+        {
+            return Message.Contains(Substring);
+        }
     }
 }
