075764d3bb76d4a3f4888101f8ca067ef709b61f
Ali <mohamadalitellawi@gmail.com>
2021-09-19 10:04:13 +0300

    uddate to use xml comments and return multiple elements inside dynamo

--- MatZeroTouch/HelloWorld.cs
+++ MatZeroTouch/HelloWorld.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MatZeroTouch
 {
-
+    // https://github.com/DynamoDS/Dynamo/wiki/Zero-Touch-Plugin-Development
 
     public class HelloWorld
     {
@@ -26,6 +26,54 @@ namespace MatZeroTouch
         public bool Contains(string Substring)
         {
             return Message.Contains(Substring);
+        }
+
+        /// <summary>
+        /// This is an example node demonstrating how to use the Zero Touch import mechanism.
+        /// It returns the input number multiplied by 2.
+        /// </summary>
+        /// <param name="inputNumber">Number that will get multiplied by 2</param>
+        /// <returns name="outputNumber">The result of the input number multiplied by 2</returns>
+        /// <search>
+        /// example, multiply, math
+        /// </search>
+        public static double MultByTwo(double inputNumber)
+        {
+            return inputNumber * 2.0;
+        }
+
+
+        //Returning multiple values
+        //The first step is to reference "DynamoServices.dll" in your project, found in the Dynamo install location, and add using Autodesk.DesignScript.Runtime; to the top of your C# script.
+
+        [Autodesk.DesignScript.Runtime.MultiReturn(new[] { "add", "mult" })]
+        public static Dictionary<string, object> ReturnMultiExample(double a, double b)
+        {
+            return new Dictionary<string, object>
+                {
+                    { "add", (a + b) },
+                    { "mult", (a * b) }
+                };
+        }
+
+        //Example 2: This node will display "thing one" and "thing two" in its output ports but it will show "thing 1" and "thing 2" in the node preview.
+        /// <summary>
+        /// The names of the output ports  
+        /// match the XML returns tag only if they include descriptions.
+        /// Otherwise the output ports will match the attribute names.
+        /// The returned dictionary displayed in the node preview is displayed
+        /// in the order of its keys as specified in the MultiReturn attribute.
+        /// </summary>
+        /// <returns name="thing one">first thing</returns>
+        /// <returns name="thing two">second thing</returns>
+        [Autodesk.DesignScript.Runtime.MultiReturn(new[] { "thing 1", "thing 2" })]
+        public static Dictionary<string, List<string>> MultiReturnExample2()
+        {
+            return new Dictionary<string, List<string>>()
+            {
+                { "thing 1", new List<string>{"apple", "banana", "cat"} },
+                { "thing 2", new List<string>{"Tywin", "Cersei", "Hodor"} }
+            };
         }
     }
 }
--- MatZeroTouch/RevitWall.cs
+++ MatZeroTouch/RevitWall.cs
@@ -31,10 +31,37 @@ namespace MatZeroTouch
 
             //Wall newWall = Wall.Create(Autodesk.Revit.DB.Document document, Autodesk.Revit.DB.Curve curve, Autodesk.Revit.DB.ElementId levelId, bool structural);
 
+            // A transaction is a context required in Revit in order to make any 
+            // modifications to a model – this includes the creation or deletion of elements
+            // Without a transaction in out method would throw an exception as we are attempting to modify the document.
+
             TransactionManager.Instance.EnsureInTransaction(document);
             Wall newWall = Wall.Create(document, line, levelElementId, false);
             TransactionManager.Instance.TransactionTaskDone();
 
+            /*
+             * Tip: It is possible to use the Revit API’s Transaction class for the same purpose, however it is 
+                    recommended to use the TransactionManager class from Dynamo’s RevitServices library as It 
+                    saves you the effort of having to dispose your objects (aka garbage collection), plus it helps to 
+                    keep both Revit and Dynamo in more consistent states. 
+             */
+
+            //you'll need to manually manage the geometry resources created in your functions which are not returned out of your functions.
+            //Any resources returned out of your functions will be managed by the Dynamo engine.
+            document.Dispose();line.Dispose();
+
+
+
+
+            /*
+             *  to make the Revit wall 
+                element Dynamo-compatible: we need to wrap it in Dynamo’s Revit.Elements.Element wrapper 
+                class. This library is an interop library which delivers all of Dynamo’s Revit nodes you see in its 
+                node library, as well as helper functions to wrap and unwrap elements from Revit.
+             * The method we call to ‘wrap’ Revit elements into the Dynamo wrapper class is ToDSType(). It 
+                takes a bool input, where true is used if the element exists in Revit, or false if the element is 
+                being instantiated by our code
+             * */
             return newWall.ToDSType(false);
         }
     }
